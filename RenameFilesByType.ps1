07b4a9e0e29b537b2b0dc6c6eb1a688b4fa17df2
cjsander <chrystalsander@hotmail.com>
2023-10-02 14:17:09 -0500

    Rename Files Sequentially Var Type

---
+++ RenameFilesByType.ps1
@@ -0,0 +1,22 @@
+﻿$currentDate = Get-Date -format "F"
+$path =  "C:\Temp\Logs\log_ps_$currentDate.txt"
+
+#Set-ExecutionPolicy Unrestricted -Scope CurrentUser
+
+Start-Transcript -Path $path -Append
+
+Write-Output "SCRIPT: SEQUENTIAL RENAME BY FILETYPE"
+
+Write-Output "START: "$currentDate
+
+$TargetPath = "C:\Users\chrys\Desktop\TMP_TJX"
+$Prefix = "2023TJX_"
+$Files = Get-ChildItem –Path $TargetPath -Exclude *.PDF -File -Recurse -ErrorAction SilentlyContinue
+$PadTo = '0'*$Files.Count.ToString().Length
+$ID = 1
+$Files | ForEach { Rename-Item -LiteralPath $_.fullname -NewName ($prefix + ($id++).tostring($PadTo) + $_.extension) }
+
+$currentDate = Get-Date -format "F"
+Write-Output "STOP: "$currentDate
+
+Stop-Transcript
